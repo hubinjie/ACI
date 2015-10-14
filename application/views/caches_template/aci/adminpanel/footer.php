d2aef821a0aaf327f04980fcab2520049449c026
alex-mac <alex-mac@alex-macmatoMacBook-Pro.local>
2015-10-14 22:32:01 +0800

    no message

--- application/views/caches_template/aci/adminpanel/footer.php
+++
@@ -1,3 +0,0 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed.'); ?><?php defined('IN_ADMIN') or exit('No permission resources.'); ?>
-</body>
-</html>
--- application/views/public/footer_view.php
+++
@@ -1,4 +0,0 @@
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="http://v3.bootcss.com/assets/js/ie10-viewport-bug-workaround.js"></script>
-</body>
-</html>
