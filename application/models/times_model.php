0f420340f9abc6712b18b5bac931bdd78ce08f90
alex-mac <alex-mac@alex-macmatoMacBook-Pro.local>
2015-10-23 14:19:57 +0800

    no message

--- application/models/times_model.php
+++
@@ -1,7 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Times_model extends Base_Model {
-	public function __construct() {
-		$this->table_name = 'times';
-		parent::__construct();
-	}
-}
